73aeb88ea8415c88229dbc3302fa8184b4fc45be
juanmoretime <jjime368@mtroyal.ca>
2020-03-30 20:51:58 -0600

    filter for year functionality

--- components/YearInput.js
+++ components/YearInput.js
@@ -46,19 +46,35 @@ class YearInput extends React.Component{
         this.setState({[e.target.name]: e.target.value});
     }
     handleSearch=(e)=>{
-
+        const type = e.target.year.value;
+        let url = ''
+        if(type=="between"){
+            const before =this.state.before;
+            const after =this.state.after;
+            url = `http://localhost:8080/api/find/year/${after}/${before}`;
+            //this.props.filterFunction(url);
+        }
+        else{
+            const year = this.state[e.target.year.value];
+            url = `http://localhost:8080/api/find/year/${type}/${year}`
+        }
+        this.props.filterFunction(url);
+        e.preventDefault();
+        e.stopPropagation();
+        e.nativeEvent.stopImmediatePropagation();
     }
     render(){
         return(
+            <Form onSubmit={this.handleSearch}>
             <Form.Group controlId={this.props.controlId}>
                 <Form.Row>
                     <Form.Label column sm={2} md={2} lg={2} className="text-left align-middle align-text-middle">
                             Year
                     </Form.Label>
                     <Col sm={10} md={10} lg={2}>
-                        <Form.Check type="radio" id="before" name="year" label="After" onClick={this.onClick}/>
-                        <Form.Check type="radio" id="after" name="year" label="Before" onClick={this.onClick}/>
-                        <Form.Check type="radio" id="between" name="year" label="Between" onClick={this.onClick}/>
+                        <Form.Check type="radio" id="after" name="year" label="After" value="after" onClick={this.onClick}/>
+                        <Form.Check type="radio" id="before" name="year" label="Before" value="before" onClick={this.onClick}/>
+                        <Form.Check type="radio" id="between" name="year" label="Between" value="between" onClick={this.onClick}/>
                     </Col>
                     <Col sm={10} md={10} lg={3} >
                         <Form.Control disabled={this.state.afterDisabled} onChange={this.onChange} type="number" name="after" value={this.state.after}/>
@@ -69,10 +85,11 @@ class YearInput extends React.Component{
                 </Form.Row>
                 <Form.Row>
                     <Col>
-                        <Button disabled={this.state.disabled} onClick={this.handleSearch}>Search</Button>
+                        <Button disabled={this.state.disabled} type="submit">Search</Button>
                     </Col>
                 </Form.Row>
             </Form.Group>
+            </Form>
         );
     }
 }
